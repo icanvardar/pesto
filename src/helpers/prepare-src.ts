a18b12be896e89460bd1e7ac15b85490c32f9120
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 13:52:52 +0000

    Create prepare-src.ts

---
+++ src/helpers/prepare-src.ts
@@ -0,0 +1,23 @@
+import { mkdirSync, statSync, writeFileSync } from "fs";
+import path from "path";
+import { pseudoCodes } from "../constants";
+
+export function prepareSrc(projectPath: string, isTs: boolean) {
+  const indexFilePath = isTs ? ".ts" : ".js";
+  const pseudoCode = isTs
+    ? pseudoCodes.pseudoCodeForTypescript
+    : pseudoCodes.pseudoCodeForBasic;
+  const srcFolderPath = path.resolve(projectPath, "src");
+  const indexFileFullPath = path.resolve(srcFolderPath, `index${indexFilePath}`);
+
+  try {
+    mkdirSync(srcFolderPath);
+    writeFileSync(
+      indexFileFullPath,
+      pseudoCode
+    );
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
