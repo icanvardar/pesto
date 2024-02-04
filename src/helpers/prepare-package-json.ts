a31e55ebcf49a6558709357b40b326a2d2026d6f
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 12:58:39 +0000

    Create prepare-package-json.ts

---
+++ src/helpers/prepare-package-json.ts
@@ -0,0 +1,31 @@
+import { writeFileSync } from "fs";
+import path from "path";
+import { PackageJson } from "type-fest";
+import { dependencies } from "../constants";
+
+export function preparePackageJson(projectName: string, projectPath: string, isTs: string) {
+  const packageJson: PackageJson = {
+    name: projectName,
+    description: "My node package!",
+    version: "0.1.0",
+    main: "dist/index.js",
+    files: ["dist/*"],
+    license: "MIT",
+    devDependencies: {...dependencies.basicDevDependencies},
+  };
+
+  if (isTs) {
+    packageJson.devDependencies = {...dependencies.tsDevDependencies};
+    packageJson.types = "dist/index.d.ts",
+  }
+
+  try {
+    writeFileSync(
+      path.resolve(projectPath, "package.json"),
+      JSON.stringify(packageJson, null, 2)
+    );
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
