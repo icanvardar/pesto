d87414eb7a17cbe166911217687f9284be6ff316
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 12:58:56 +0000

    Create run-script.ts

---
+++ src/helpers/run-script.ts
@@ -0,0 +1,18 @@
+import util from "util";
+import cp from "child_process";
+
+const exec = util.promisify(cp.exec);
+
+export async function runScript(command: string) {
+  try {
+    const { stdout, stderr } = await exec(command);
+
+    if (stderr) {
+      throw new Error(stderr);
+    }
+
+    return stdout;
+  } catch (err: unknown) {
+    throw new Error(err as unknown as string);
+  }
+}
