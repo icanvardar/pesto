354e71577f4ad76303f5875242df1084c17f5c18
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 16:10:18 +0000

    Update generate-path.ts

--- src/helpers/generate-path.ts
+++ src/helpers/generate-path.ts
@@ -4,12 +4,12 @@ import path from "path";
  * Generates an absolute path from the given path.
  * @param {string} pathToCheck - The path to be resolved.
  * @returns {string} The resolved absolute path.
+ * @throws {Error} Throws an error if any preparation step fails.
  */
 export function generatePath(pathToCheck: string): string {
     try {
         return path.resolve(pathToCheck);
-    } catch (error) {
-        console.error("Error occurred while generating path:", error);
-        process.exit(1);
+    } catch (err) {
+        throw new Error(err as string);
     }
 }
