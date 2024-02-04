84ce38031637fc99fbc066cf84834fd3843b39a6
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 13:52:30 +0000

    Update types.ts

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -14,3 +14,17 @@ export interface Args {
   template: TemplateType;
   verbose: boolean;
 }
+
+export type Dependency = Record<string, string>;
+
+export interface Dependencies {
+  [key: string]: Dependency;
+  basicDevDependencies: Dependency;
+  tsDevDependencies: Dependency;
+}
+
+export type PseudoCode = string;
+
+export interface PseudoCodes {
+  [key: string]: string;
+}
