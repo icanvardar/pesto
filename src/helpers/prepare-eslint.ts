2474832cd599fa46bdc7d7277c875b063892e585
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 16:44:42 +0000

    Update prepare-eslint

--- src/helpers/prepare-eslint.ts
+++ src/helpers/prepare-eslint.ts
@@ -5,9 +5,10 @@ import { eslintOptions } from "../constants";
 /**
  * Prepares ESLint configuration files in the project directory.
  * @param {string} projectPath - The path to the project directory.
+ * @returns {string[]} Path of both .eslintrc and .eslintignore
  * @throws {Error} Throws an error if any preparation step fails.
  */
-export function prepareEslint(projectPath: string): void {
+export function prepareEslint(projectPath: string): string[] {
     const eslintrcFullPath = path.resolve(projectPath, ".eslintrc");
     const eslintIgnoreFullPath = path.resolve(projectPath, ".eslintignore");
     const eslintIgnore = `
@@ -20,6 +21,8 @@ export function prepareEslint(projectPath: string): void {
     try {
         writeFileSync(eslintrcFullPath, JSON.stringify(eslintOptions));
         writeFileSync(eslintIgnoreFullPath, eslintIgnore, { encoding: "utf-8" });
+
+        return [eslintrcFullPath, eslintIgnoreFullPath];
     } catch (err: unknown) {
         throw new Error(err as string);
     }
