c11871427a99df00725cfaf86cc5a2bb3e39ea14
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 14:28:31 +0000

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
 import dependencies from "./dependencies";
 import pseudoCodes from "./pseudoCodes";
+import eslintOptions from "./eslint-options";
+import prettierOptions from "./prettier-options";
 
-export { dependencies, pseudoCodes };
+export { dependencies, pseudoCodes, eslintOptions, prettierOptions };
