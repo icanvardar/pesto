0c8f55c71401bd56dcce5ec09116a53ee4ff1bab
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 14:28:26 +0000

    Create prettier-options.ts

---
+++ src/constants/prettier-options.ts
@@ -0,0 +1,11 @@
+const prettierOptions = {
+  printWidth: 100,
+  tabWidth: 4,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "none",
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
+
+export default prettierOptions;
