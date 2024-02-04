52f43af535ff51ea8a1d1df0f1af3910116c3817
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 12:58:44 +0000

    Create prepare-path.ts

---
+++ src/helpers/prepare-path.ts
@@ -0,0 +1,13 @@
+import { existsSync, mkdirSync } from "fs";
+import { runScript } from "./run-script";
+
+// prepare path
+export async function preparePath(projectPath: string): Promise<void> {
+  const folderExist = existsSync(projectPath);
+
+  if (!folderExist) {
+    mkdirSync(projectPath);
+  } else {
+    await runScript(`rm -rf ${projectPath}/*`);
+  }
+}
