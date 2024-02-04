52a07b0ff3bc0f9812bdd3b54da9b8644b864e87
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 13:53:14 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,15 @@
 import path from "path";
-import { getArgs, preparePath, prepareTsConfig } from "./helpers";
+import { getArgs, prepareTemplate } from "./helpers";
 
 async function init() {
   const { projectPath, template, verbose } = await getArgs();
   const projectName: string = path.basename(projectPath);
+  let isTs: boolean = template === "typescript" ? true : false;
 
   try {
-    await preparePath(projectPath);
+    await prepareTemplate(projectName, projectPath, isTs);
   } catch(err: unknown) {
     throw new Error(err as unknown as string);
-  }
-
-  switch (template) {
-    case "basic":
-      console.log("basic template!");
-      break;
-    case "typescript":
-      prepareTsConfig(projectPath);
-      break;
-    default:
-      console.log("default");
-      break;
   }
 }
 
