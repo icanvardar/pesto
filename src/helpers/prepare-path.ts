c82faac333eeafad3a23562799cc42b4aae8e712
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 16:10:57 +0000

    Update prepare-path.ts

--- src/helpers/prepare-path.ts
+++ src/helpers/prepare-path.ts
@@ -5,13 +5,18 @@ import { runScript } from "./run-script";
  * Prepares the path for the project.
  * @param {string} projectPath - The path to the project directory.
  * @returns {Promise<void>} A Promise that resolves once the path is prepared.
+ * @throws {Error} Throws an error if any preparation step fails.
  */
 export async function preparePath(projectPath: string): Promise<void> {
     const folderExist = existsSync(projectPath);
 
-    if (!folderExist) {
-        mkdirSync(projectPath);
-    } else {
-        await runScript(`rm -rf ${projectPath}/*`);
+    try {
+        if (!folderExist) {
+            mkdirSync(projectPath);
+        } else {
+            await runScript(`rm -rf ${projectPath}/*`);
+        }
+    } catch (err: unknown) {
+        throw new Error(err as string);
     }
 }
