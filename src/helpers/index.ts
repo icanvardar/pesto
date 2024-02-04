8769443272dd173f46d247a6fbf8fa8444e7ae0c
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 14:28:53 +0000

    Update index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,10 @@
 import { generatePath } from "./generate-path";
 import { getArgs } from "./get-args";
+import { prepareEslint } from "./prepare-eslint";
+import { prepareHusky } from "./prepare-husky";
 import { preparePackageJson } from "./prepare-package-json";
 import { preparePath } from "./prepare-path";
+import { preparePrettier } from "./prepare-prettier";
 import { prepareSrc } from "./prepare-src";
 import { prepareTemplate } from "./prepare-template";
 import { prepareTsConfig } from "./prepare-ts-config";
@@ -10,8 +13,11 @@ import { runScript } from "./run-script";
 export {
   generatePath,
   getArgs,
+  prepareEslint,
+  prepareHusky,
   preparePackageJson,
   preparePath,
+  preparePrettier,
   prepareSrc,
   prepareTemplate,
   prepareTsConfig,
