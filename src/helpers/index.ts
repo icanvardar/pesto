3d79551355ac1c655a5f20ca492fefce43146dac
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 12:59:08 +0000

    Create index.ts

---
+++ src/helpers/index.ts
@@ -0,0 +1,7 @@
+import { generatePath } from "./generate-path";
+import { getArgs } from "./get-args";
+import { preparePath } from "./prepare-path";
+import { prepareTsConfig } from "./prepare-ts-config";
+import { runScript } from "./run-script";
+
+export { generatePath, getArgs, preparePath, prepareTsConfig, runScript };
