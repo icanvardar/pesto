1f2d9b69e1d0ba3e4c43fe554d281012dd517c9f
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 14:28:50 +0000

    Create prepare-husky.ts

---
+++ src/helpers/prepare-husky.ts
@@ -0,0 +1,15 @@
+import { mkdirSync, writeFileSync } from "fs";
+import path from "path";
+
+export function prepareHusky(projectPath: string) {
+  const huskyFolder = path.resolve(projectPath, ".husky");
+  const preCommitFileFullPath = path.resolve(huskyFolder, "pre-commit");
+
+  try {
+    mkdirSync(huskyFolder);
+    writeFileSync(preCommitFileFullPath, "npx lint-staged");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
