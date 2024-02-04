9e5bf7a6c3f65c9b1f6196638f541f07a459d3b1
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 16:11:05 +0000

    Update prepare-husky.ts

--- src/helpers/prepare-husky.ts
+++ src/helpers/prepare-husky.ts
@@ -4,6 +4,7 @@ import path from "path";
 /**
  * Prepares Husky configuration for pre-commit hooks in the project directory.
  * @param {string} projectPath - The path to the project directory.
+ * @throws {Error} Throws an error if any preparation step fails.
  */
 export function prepareHusky(projectPath: string): void {
     const huskyFolder = path.resolve(projectPath, ".husky");
@@ -12,8 +13,7 @@ export function prepareHusky(projectPath: string): void {
     try {
         mkdirSync(huskyFolder);
         writeFileSync(preCommitFileFullPath, "npx lint-staged");
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
+    } catch (err: unknown) {
+        throw new Error(err as string);
     }
 }
