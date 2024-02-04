2bde0de4e29ee0445116bfbdad5039640eaf92ed
Can Vardar <ismailcanvardar@gmail.com>
2024-02-04 13:53:11 +0000

    Update index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,19 @@
 import { generatePath } from "./generate-path";
 import { getArgs } from "./get-args";
+import { preparePackageJson } from "./prepare-package-json";
 import { preparePath } from "./prepare-path";
+import { prepareSrc } from "./prepare-src";
+import { prepareTemplate } from "./prepare-template";
 import { prepareTsConfig } from "./prepare-ts-config";
 import { runScript } from "./run-script";
 
-export { generatePath, getArgs, preparePath, prepareTsConfig, runScript };
+export {
+  generatePath,
+  getArgs,
+  preparePackageJson,
+  preparePath,
+  prepareSrc,
+  prepareTemplate,
+  prepareTsConfig,
+  runScript
+};
